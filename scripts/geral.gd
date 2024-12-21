cb6776852bc9bf615278b3daba273d27b69a6f11
Marcos Sousa <104542682+YakumaKuramy@users.noreply.github.com>
2024-12-21 11:37:33 -0300

    update

--- scripts/geral.gd
+++ scripts/geral.gd
@@ -1,42 +1,51 @@
 extends Node2D
 
+@export var diversão 	: int = 100
+@export var fome 		: int = 100
+@export var felicidade 	: int = 60
+@export var miado 		: int = 10
 
-@export var tempo_miado : int = 10
-@export var tempo_comida : int = 60
-@export var pode_comer : bool = false
+@export var pode_comer 	: bool = false
 @export var pode_morrer : bool = false
 
 @onready var gato : AnimatedSprite2D = $gato
 @onready var balao : Sprite2D = $balao
-@onready var t_tempo_comida : Timer = $balao/tempo_comida
 @onready var audio_miado : AudioStreamPlayer = $gato/audio_miado
 @onready var cat_trilling : AudioStreamPlayer = $gato/cat_trilling
 
+var rng = RandomNumberGenerator.new()
+
+func _ready() -> void:
+	var my_float = rng.randf_range(-10.0, 10.0)
+	var my_int = rng.randi_range(0, 10)
+	print(my_float)
+	print(my_int)
 
 func _on_botao_pressed() -> void:
-	if tempo_comida <= 0:
-		cat_trilling.play()
-		gato.play("comendo")
-		await gato.animation_finished
-		gato.play("idle")
-		tempo_comida = 60
+	#if tempo_comida <= 0:
+	cat_trilling.play()
+	gato.play("comendo")
+	await gato.animation_finished
+	gato.play("idle")
+	fome = 100
 
 
 func _on_miado_timer_timeout() -> void:
-	if tempo_miado <= 0:
+	if miado <= 0:
 		audio_miado.play()
-		tempo_miado = 10
+		miado = 10
 		gato.play("miado")
 		await gato.animation_finished
 		gato.play("idle")
-	tempo_miado -= 1
+	print("Tempo para miar: ", miado)
+	miado -= 1
 
 
 func _on_tempo_comida_timeout() -> void:
-	if tempo_comida <= 0:
+	if fome <= 0:
 		balao.show()
 		await get_tree().create_timer(3).timeout
 		balao.hide()
-		tempo_comida = 60
-	tempo_comida -= 1
-	print(tempo_comida)
+		fome = 100
+	fome -= 1
+	print("Nivel de fome: ", fome)
